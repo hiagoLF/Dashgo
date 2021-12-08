69b1a8da7ea0c94101242678a6947249e3e75546
hiagoLF <hiagolof@gmail.com>
2021-12-08 09:07:07 -0300

    Users List Layout

---
+++ src/pages/users/index.tsx
@@ -0,0 +1,143 @@
+import React from "react";
+import {
+  Box,
+  Flex,
+  Heading,
+  Button,
+  Icon,
+  Table,
+  Thead,
+  Tr,
+  Th,
+  Checkbox,
+  Tbody,
+  Td,
+  Text,
+} from "@chakra-ui/react";
+import { Sidebar } from "../../components/SideBar";
+import { RiAddLine, RiPencilLine } from "react-icons/ri";
+import Header from "../../components/Header";
+
+const UserList: React.FC = () => {
+  return (
+    <Box>
+      <Header />
+
+      <Flex w="100%" my="6" maxW={1480} mx="auto" px="6">
+        <Sidebar />
+
+        <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+          <Flex mb="8" justify="space-between" align="center">
+            <Heading size="lg" fontWeight="normal">
+              Usuários
+            </Heading>
+
+            <Button
+              as="a"
+              size="sm"
+              fontSize="sm"
+              colorScheme="pink"
+              leftIcon={<Icon as={RiAddLine} fontSize='20'/>}
+            >
+              Criar Novo
+            </Button>
+          </Flex>
+
+          <Table colorScheme="whiteAlpha">
+            <Thead>
+              <Tr>
+                <Th px="6" color="gray.300" width="8">
+                  <Checkbox colorScheme="pink" />
+                </Th>
+                <Th>Usuário</Th>
+                <Th>Data de Cadastro</Th>
+                <Th width="8"></Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Hiago Leão</Text>
+                    <Text fontSize="sm" color="gray.300">
+                      hiagolof@gmail.com
+                    </Text>
+                  </Box>
+                </Td>
+                <Td>05 de Abril de 1997</Td>
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="purple"
+                    leftIcon={<Icon as={RiPencilLine} fontSize='16'/>}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+              
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Hiago Leão</Text>
+                    <Text fontSize="sm" color="gray.300">
+                      hiagolof@gmail.com
+                    </Text>
+                  </Box>
+                </Td>
+                <Td>05 de Abril de 1997</Td>
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="purple"
+                    leftIcon={<Icon as={RiPencilLine} fontSize='16'/>}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Hiago Leão</Text>
+                    <Text fontSize="sm" color="gray.300">
+                      hiagolof@gmail.com
+                    </Text>
+                  </Box>
+                </Td>
+                <Td>05 de Abril de 1997</Td>
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="sm"
+                    colorScheme="purple"
+                    leftIcon={<Icon as={RiPencilLine} fontSize='16'/>}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+            </Tbody>
+          </Table>
+        </Box>
+      </Flex>
+    </Box>
+  );
+};
+
+export default UserList;
